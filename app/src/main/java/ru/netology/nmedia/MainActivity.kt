5f808380568ebc4e922381762d87f049c762dd9c
alica_suslova <alisasuslova1408@gmail.com>
2024-04-06 21:12:21 +0300

    06/04/24 mvvm сердце меняется, репост работает!

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -77,6 +77,9 @@ class MainActivity : AppCompatActivity() {
                 author.text = post.author
                 published.text = post.published
                 content.text = post.content
+                likesCount.text = post.likes.toString()
+                sharesCount.text = post.shares.toString()
+
                 likes.setImageResource(
                     if (post.likedByMe) R.drawable.heart_like_red_20 else R.drawable.heart_like_20
                 )
@@ -85,22 +88,12 @@ class MainActivity : AppCompatActivity() {
         binding.likes.setOnClickListener {
             viewModel.like()
         }
+
+        binding.shares.setOnClickListener {
+            viewModel.share()
+        }
     }
 }
 
-/*fun shortNote(int: Int): String {
 
-    val temp: BigDecimal = int.toBigDecimal().divide(1_000.toBigDecimal())
-    val temp1: BigDecimal = int.toBigDecimal().divide(1_000.toBigDecimal())
-    val temp2: BigDecimal = int.toBigDecimal().divide(1_000_000.toBigDecimal())
-
-    return when (int) {
-        in 1..999 -> int.toString()
-        in 1_000..9_999 -> String.format("%.1f", temp.setScale(1, RoundingMode.FLOOR)) + "K"
-        in 10_000..999_999 -> String.format("%.0f", temp1.setScale(0, RoundingMode.FLOOR)) + "K"
-        else -> {
-            String.format("%.1f", temp2.setScale(1, RoundingMode.FLOOR)) + "M"
-        }
-    }
-}*/
 
--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
@@ -3,6 +3,8 @@ package ru.netology.nmedia.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import ru.netology.nmedia.dto.Post
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class PostRepositoryInMemoryImpl : PostRepository {
     private var post = Post(
@@ -10,13 +12,37 @@ class PostRepositoryInMemoryImpl : PostRepository {
         author = "Нетология. Университет интернет-профессий будущего",
         content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
         published = "21 мая в 18:36",
-        likedByMe = false
+        likedByMe = false,
+        likes = 10999,
+        shares = 6
     )
     private val data = MutableLiveData(post)
 
     override fun get(): LiveData<Post> = data
     override fun like() {
-        post = post.copy(likedByMe = !post.likedByMe)
+        post = post.copy(likedByMe = !post.likedByMe) //мб здесь изменение числовых значений
         data.value = post
+    }
+
+    override fun share() {
+        var localSharesCount = post.shares
+        post = post.copy(shares = localSharesCount + 1)
+        data.value = post
+    }
+}
+
+fun shortNote(int: Int): String {
+
+    val temp: BigDecimal = int.toBigDecimal().divide(1_000.toBigDecimal())
+    val temp1: BigDecimal = int.toBigDecimal().divide(1_000.toBigDecimal())
+    val temp2: BigDecimal = int.toBigDecimal().divide(1_000_000.toBigDecimal())
+
+    return when (int) {
+        in 1..999 -> int.toString()
+        in 1_000..9_999 -> String.format("%.1f", temp.setScale(1, RoundingMode.FLOOR)) + "K"
+        in 10_000..999_999 -> String.format("%.0f", temp1.setScale(0, RoundingMode.FLOOR)) + "K"
+        else -> {
+            String.format("%.1f", temp2.setScale(1, RoundingMode.FLOOR)) + "M"
+        }
     }
 }
