4b84b1eaf572024dc215aba73100dd3e1a9dfcf1
alica_suslova <alisasuslova1408@gmail.com>
2024-04-13 19:56:13 +0300

    13/04/24 adapter/holder

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,39 +1,26 @@
-package ru.netology.nmedia
+package ru.netology.nmedia.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.databinding.CardPostBinding
 import ru.netology.nmedia.viewmodel.PostViewModel
-import java.math.BigDecimal
-import java.math.RoundingMode
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
+        val adapter = PostsAdapter {
+            viewModel.likeById(it.id)
+        }
+        binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
-            binding.container.removeAllViews()
-            posts.map {post ->
-                CardPostBinding.inflate(layoutInflater, binding.container, true).apply {
-                    author.text = post.author
-                    published.text = post.published
-                    content.text = post.content
-                    likes.setImageResource(
-                        if (post.likedByMe) R.drawable.heart_like_red_20 else R.drawable.heart_like_20
-                    )
-                    likes.setOnClickListener {
-                        viewModel.likeById(post.id)
-                    }
-                }.root
-            }
+            adapter.list = posts
         }
     }
 }
-
 
---
+++ app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
@@ -0,0 +1,52 @@
+package ru.netology.nmedia.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.CardPostBinding
+import ru.netology.nmedia.dto.Post
+
+typealias OnLikeListener = (post: Post) -> Unit
+
+class PostsAdapter(private val onLikeListener: OnLikeListener) : RecyclerView.Adapter<PostViewHolder>() {
+    var list = emptyList<Post>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val binding = CardPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PostViewHolder(binding, onLikeListener)
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        val post = list[position]
+        holder.bind(post)
+    }
+
+    override fun getItemCount(): Int = list.size
+}
+
+class PostViewHolder(
+    private val binding: CardPostBinding,
+    private val onLikeListener: OnLikeListener
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(post: Post) {
+        binding.apply {
+            author.text = post.author
+            published.text = post.published
+            content.text = post.content
+            likes.setImageResource(
+                if (post.likedByMe) R.drawable.heart_like_red_20 else R.drawable.heart_like_20
+            )
+            /*if (post.likedByMe) {
+                likes.setImageResource(R.drawable.ic_liked_24)
+            }*/
+            likes.setOnClickListener{
+                onLikeListener(post)
+            }
+        }
+    }
+}
