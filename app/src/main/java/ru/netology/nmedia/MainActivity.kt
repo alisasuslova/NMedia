f4e903a82b313b15bc27f9e808224e0bfc970da4
alica_suslova <alisasuslova1408@gmail.com>
2024-04-18 00:54:15 +0300

    17/04/24

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,10 +1,12 @@
 package ru.netology.nmedia
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.util.AndroidUtils
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -29,7 +31,27 @@ class MainActivity : AppCompatActivity() {
 
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
-            adapter.submitList(posts)
+            val newPost = posts.size > adapter.currentList.size  //чтобы не перескакивало вверх при нажатии на реакции
+            adapter.submitList(posts) {
+                if (newPost) {
+                    binding.list.smoothScrollToPosition(0)
+                }
+            }
+        }
+
+        binding.save.setOnClickListener {
+            val content = binding.content.text.toString()
+
+            if (content.isBlank()) {
+                Toast.makeText(this, R.string.error_empty_content, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            viewModel.changeContent(content)
+            viewModel.save()
+
+            binding.content.setText("") //чтобы поле для ввода текста отчищалось после добаления поста
+            binding.content.clearFocus() // убирает мигающий курсор
+            AndroidUtils.hideKeyboard(binding.content) // убирает клавиатуру после добавления поста
         }
     }
 }
--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,13 +1,39 @@
 package ru.netology.nmedia.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
+import kotlin.coroutines.coroutineContext
 
+private val empty = Post(
+    id = 0,
+    content = "",
+    author = "",
+    likedByMe = false,
+    published = ""
+)
 class PostViewModel : ViewModel() {
 
     private val repository: PostRepository = PostRepositoryInMemoryImpl()
     val data = repository.getAll()
+    val edited = MutableLiveData(empty)
+    fun save() {
+        edited.value?.let {
+            repository.save(it)
+        }
+        edited.value = empty
+    }
+
+    fun changeContent(text: String) {
+        edited.value?.let {
+            if (it.content != text.trim()){
+                edited.value = it.copy(content = text)
+            }
+        }
+    }
+
     fun likeById(id: Long) = repository.likeById(id)
     fun shareById(id: Long) = repository.shareById(id)
     fun removeById(id: Long) = repository.removeById(id)
