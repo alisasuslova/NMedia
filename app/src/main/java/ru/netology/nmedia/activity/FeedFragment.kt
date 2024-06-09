9bccd1cf6c240f69d9d59dd115f0645f91e7f24d
alica_suslova <alisasuslova1408@gmail.com>
2024-06-09 15:34:13 +0300

    09/06/24 activity -> fragment

---
+++ app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
@@ -0,0 +1,219 @@
+package ru.netology.nmedia.activity
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.launch
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatViewInflater
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainer
+import androidx.fragment.app.viewModels
+import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.OnInteractionListener
+import ru.netology.nmedia.adapter.PostsAdapter
+
+import ru.netology.nmedia.databinding.FragmentFeedBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.viewmodel.PostViewModel
+
+class FeedFragment : Fragment() {
+
+    private val viewModel: PostViewModel by viewModels(
+        ownerProducer = ::requireParentFragment
+    )
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentFeedBinding = FragmentFeedBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        //регистрация контракта на создание нового поста
+        val newPostLauncher = registerForActivityResult(NewPostContract) {
+            val result = it ?: return@registerForActivityResult
+            viewModel.changeContentAndSave(result)
+        }
+
+        //регистрация контракта на редактирование
+        val editPostLauncher = registerForActivityResult(EditPostContract) {
+            val result = it ?: return@registerForActivityResult
+            viewModel.changeContentAndSave(result)
+        }
+
+
+        val adapter = PostsAdapter(object : OnInteractionListener {
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onShare(post: Post) {
+                //сама механика передачи данных в другие прилодения
+                val intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT, post.content)
+                }
+                //больше выбор приложений
+                val shareIntent =
+                    Intent.createChooser(intent, getString(R.string.chooser_share_post))
+                startActivity(shareIntent)
+
+                // добавить подсчет количества!
+                viewModel.shareById(post.id)
+            }
+
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+
+            override fun playVideo(post: Post) {
+                TODO("Not yet implemented")
+            }
+
+            /*override fun playVideo(post: Post) {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(post.video))
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+                viewModel.playVideo(post.id)
+                viewModel.editCancel()
+            }*/
+
+            override fun openPost(post: Post) {
+                viewModel.playVideo(post.id)
+                viewModel.editCancel()
+            }
+
+        })
+
+                binding.list.adapter = adapter
+                viewModel.data.observe(viewLifecycleOwner) { posts ->
+                    val newPost =
+                        posts.size > adapter.currentList.size  //чтобы не перескакивало вверх при нажатии на реакции
+                    adapter.submitList(posts) {
+                        if (newPost) {
+                            binding.list.smoothScrollToPosition(0)
+                        }
+                    }
+                }
+
+
+                binding.save.setOnClickListener {
+            newPostLauncher.launch()
+        }
+
+                viewModel.edited.observe(viewLifecycleOwner) {
+
+            if (it.id != 0L) {
+                editPostLauncher.launch(it.content)
+            }
+        }
+
+
+        return binding.root
+    }
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel: PostViewModel by viewModels()
+
+        //регистрация контракта на создание нового поста
+        val newPostLauncher = registerForActivityResult(NewPostContract) {
+            val result = it ?: return@registerForActivityResult
+            viewModel.changeContentAndSave(result)
+        }
+
+        //регистрация контракта на редактирование
+        val editPostLauncher = registerForActivityResult(EditPostContract) {
+            val result = it ?: return@registerForActivityResult
+            viewModel.changeContentAndSave(result)
+        }
+
+
+        val adapter = PostsAdapter(object : OnInteractionListener {
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onShare(post: Post) {
+
+                //сама механика передачи данных в другие прилодения
+                val intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT, post.content)
+                }
+                //больше выбор приложений
+                val shareIntent =
+                    Intent.createChooser(intent, getString(R.string.chooser_share_post))
+                startActivity(shareIntent)
+
+                // добавить подсчет количества!
+                viewModel.shareById(post.id)
+            }
+
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+
+            override fun playVideo(post: Post) {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(post.video))
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+                viewModel.playVideo(post.id)
+                viewModel.editCancel()
+            }
+
+            override fun openPost(post: Post) {
+                viewModel.playVideo(post.id)
+                viewModel.editCancel()
+            }
+
+        }
+        )
+
+        binding.list.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            val newPost =
+                posts.size > adapter.currentList.size  //чтобы не перескакивало вверх при нажатии на реакции
+            adapter.submitList(posts) {
+                if (newPost) {
+                    binding.list.smoothScrollToPosition(0)
+                }
+            }
+        }
+
+
+        binding.save.setOnClickListener {
+            newPostLauncher.launch()
+        }
+
+        viewModel.edited.observe(this) {
+
+            if (it.id != 0L) {
+                editPostLauncher.launch(it.content)
+            }
+        }
+    }*/
+
+}
