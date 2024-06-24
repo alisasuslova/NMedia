6463c3c4a734dfe4608ae54e14731951eab3304e
alica_suslova <alisasuslova1408@gmail.com>
2024-06-24 20:32:28 +0300

    24/06/24 new fragment

--- app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
@@ -1,18 +1,19 @@
 package ru.netology.nmedia.activity
 
 import android.content.Intent
-import android.content.Intent.ACTION_SEND
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.google.android.material.snackbar.Snackbar
 import ru.netology.nmedia.R
-import ru.netology.nmedia.databinding.ActivityIntentHandlerBinding
-import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.activity.NewPostFragment.Companion.textArg
+import ru.netology.nmedia.databinding.ActivityAppBinding
 
-class IntentHandlerActivity : AppCompatActivity() {
+
+class AppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityIntentHandlerBinding.inflate(layoutInflater)
+        val binding = ActivityAppBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         //проверяем входящий intent
@@ -28,7 +29,14 @@ class IntentHandlerActivity : AppCompatActivity() {
                         finish()  //действие по кнопке "ОК"
                     }
                         .show() //показать Snackbar
+                    return@let
                 }
+
+                findNavController(R.id.nav_host).navigate(
+                    R.id.action_feedFragment_to_newPostFragment,
+                    Bundle().apply {
+                        textArg = text
+                    })
             }
         }
     }
---
+++ app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
@@ -0,0 +1,111 @@
+package ru.netology.nmedia.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.OnInteractionListener
+import ru.netology.nmedia.adapter.PostsAdapter
+import ru.netology.nmedia.databinding.FragmentFeedBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.viewmodel.PostViewModel
+
+class FeedFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentFeedBinding.inflate(inflater, container, false)
+
+        val viewModel: PostViewModel by viewModels(
+            ownerProducer = :: requireParentFragment
+        )
+
+
+        val adapter = PostsAdapter(object : OnInteractionListener {
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onShare(post: Post) {
+
+                //сама механика передачи данных в другие прилодения
+                val intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT, post.content)
+                }
+                //больше выбор приложений
+                val shareIntent =
+                    Intent.createChooser(intent, getString(R.string.chooser_share_post))
+                startActivity(shareIntent)
+
+                // добавить подсчет количества!
+                viewModel.shareById(post.id)
+            }
+
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+
+            override fun playVideo(post: Post) {
+                //todo
+               /* val intent = Intent(Intent.ACTION_VIEW, Uri.parse(post.video))
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+                viewModel.playVideo(post.id)
+                viewModel.editCancel()*/
+            }
+
+            override fun openPost(post: Post) {
+                viewModel.playVideo(post.id)
+                viewModel.editCancel()
+            }
+
+        }
+        )
+
+        binding.list.adapter = adapter
+        viewModel.data.observe(viewLifecycleOwner) { posts ->
+            val newPost =
+                posts.size > adapter.currentList.size  //чтобы не перескакивало вверх при нажатии на реакции
+            adapter.submitList(posts) {
+                if (newPost) {
+                    binding.list.smoothScrollToPosition(0)
+                }
+            }
+        }
+
+
+
+        binding.save.setOnClickListener {
+            findNavController().navigate(R.id.action_feedFragment_to_newPostFragment)
+        }
+
+        viewModel.edited.observe(viewLifecycleOwner) {
+
+            //todo
+            /*if (it.id != 0L) {
+                editPostLauncher.launch(it.content)
+            }*/
+
+
+        }
+
+        return binding.root
+    }
+
+
+}
